105231d1d9dc1f6316e6e258b28ba43551d95df3
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-04-08 23:46:49 -0300

    funcionalidade jquery preciso mudar

--- Projeto/js/javascript.js
+++ Projeto/js/javascript.js
@@ -109,5 +109,20 @@ $(document).ready(function() {
 
     }, 250);
 
+    // Filtro do portifolio
+
+    $('.filter-btn').on('click', function() {
+
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if(type == 'dsg-btn') {
+            
+        }
+    });
+
 
 });
